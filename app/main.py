6c3befc67296f25e4aa3413fd20cc9f8bcd154cc
pwnFirstGit <pawanforyou18@gmail.com>
2026-05-04 00:14:43 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -23,6 +23,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",      # Your local React frontend
         "http://localhost:3000",      # Alternative local port
+        "https://adaptive-auth-frontend.onrender.com",
         "https://adaptive-auth-system.onrender.com"  # Production (if you have a frontend deployed)
     ],
     allow_credentials=True,            # Allow cookies & auth headers
