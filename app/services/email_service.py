4ede00a37d4b3e16ed013c5279db7773f4bc42f1
pwnFirstGit <pawanforyou18@gmail.com>
2026-05-03 10:49:52 +0530

    Update email_service.py

--- app/services/email_service.py
+++ app/services/email_service.py
@@ -1,23 +1,29 @@
-import resend
 import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
-resend.api_key = os.getenv("RESEND_API_KEY")
+SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
+FROM_EMAIL       = os.getenv("EMAIL_ADDRESS")
 
 def send_otp_email(to_email: str, otp: str):
     try:
-        resend.Emails.send({
-            "from": "onboarding@resend.dev",
-            "to": to_email,
-            "subject": "Your Login Verification Code",
-            "html": f"""
-                <h2>Your OTP Code</h2>
-                <p>Your verification code is:</p>
-                <h1>🔐 {otp}</h1>
-                <p>This code expires in 5 minutes.</p>
+        message = Mail(
+            from_email=FROM_EMAIL,
+            to_emails=to_email,
+            subject="Your Login Verification Code",
+            html_content=f"""
+                <h2>Adaptive Auth System</h2>
+                <p>Your one-time verification code is:</p>
+                <h1 style="color: #4F46E5;">🔐 {otp}</h1>
+                <p>This code expires in <b>5 minutes</b>.</p>
+                <p>If you didn't request this, please ignore this email.</p>
             """
-        })
+        )
+        sg = SendGridAPIClient(SENDGRID_API_KEY)
+        sg.send(message)
         print(f"[Email] OTP sent to {to_email}")
         return True
+
     except Exception as e:
         print(f"[Email Error] {e}")
         return False
